255beee12de94de175c6227ccdf20be6c8a93c97
Ben LaPaz <bdl@lifestylestaffing.com>
2018-11-28 10:40:14 -0600

    Initial commit

---
+++ MazeGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MazeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
