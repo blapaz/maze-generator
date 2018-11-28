4a7d55727fd84e114e8571278c4ea497db9a85a5
Ben LaPaz <bdl@lifestylestaffing.com>
2018-11-28 11:05:19 -0600

    Add general parameters to pass with command line parser

--- MazeGenerator/Program.cs
+++ MazeGenerator/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 
 namespace MazeGenerator
 {
     class Program
     {
+        public class Options
+        {
+            [Option('w', "width", Required = false, HelpText = "Set the width of the maze that will be generated.")]
+            public string Width { get; set; }
+
+            [Option('h', "height", Required = false, HelpText = "Set the height of the maze that will be generated.")]
+            public string Height { get; set; }
+
+            [Option('s', "size", Required = false, HelpText = "Set the size of the maze that will be generated (width,height).")]
+            public string Size { get; set; }
+
+            [Option('n', "noise", Required = false, HelpText = "Set the noise of the maze that will be generated.")]
+            public string Noise { get; set; }
+
+            [Option('p', "path", Required = false, HelpText = "Set the path where the maze file will be created.")]
+            public string Path { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    Console.WriteLine("Path Set: " + o.Path);
+                });
+
+            Console.Read();
         }
     }
 }
